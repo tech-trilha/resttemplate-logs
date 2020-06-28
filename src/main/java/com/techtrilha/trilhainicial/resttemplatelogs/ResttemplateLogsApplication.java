7f751085a8ecd5f6645206c3fbf134f5120a5b38
Felipe Miranda <felip.simi@gmail.com>
2020-06-28 11:47:50 -0300

    Preparacao: setup do projeto no Spring Initializr

---
+++ src/main/java/com/techtrilha/trilhainicial/resttemplatelogs/ResttemplateLogsApplication.java
@@ -0,0 +1,13 @@
+package com.techtrilha.trilhainicial.resttemplatelogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttemplateLogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttemplateLogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techtrilha/trilhainicial/resttemplatelogs/ResttemplateLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtrilha.trilhainicial.resttemplatelogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttemplateLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
