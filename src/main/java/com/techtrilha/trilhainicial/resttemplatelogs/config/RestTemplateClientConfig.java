68fb09c0ce83f09a11084986ac3ffb393c742c6e
Felipe Miranda <felip.simi@gmail.com>
2020-06-30 00:13:17 -0300

    Nono passo: configuracao do RestTemplate para mais de uma leitura do body

--- src/main/java/com/techtrilha/trilhainicial/resttemplatelogs/config/RestTemplateClientConfig.java
+++ src/main/java/com/techtrilha/trilhainicial/resttemplatelogs/config/RestTemplateClientConfig.java
@@ -3,6 +3,8 @@ package com.techtrilha.trilhainicial.resttemplatelogs.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -11,6 +13,8 @@ public class RestTemplateClientConfig {
   @Bean
   public RestTemplate getRestTemplate(RestTemplateBuilder builder) {
     return builder
+        .requestFactory(
+            () -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
         .additionalInterceptors(new RestTemplateErrorInterceptor())
         .build();
   }
